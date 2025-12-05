42e1a76043a119be7f2abac7ef10942967db6bf0
Yazid <yazid20032@gmail.com>
2025-12-05 19:29:36 +0100

    El Yazid - 05/12/2025

--- src/Controller/GestionEtudiantsController.php
+++ src/Controller/GestionEtudiantsController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Etudiant;
 use App\Entity\Tuteur;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -28,7 +30,7 @@ final class GestionEtudiantsController extends AbstractController{
         
         $tuteur = $repo->find($session->get('tuteur_id'));
 
-          
+        $tuteur_nom = $tuteur->getNom();
         $etudiants = $tuteur->getEtudiants();
         
 
@@ -39,9 +41,82 @@ final class GestionEtudiantsController extends AbstractController{
             array_push($tabEtudiants, ['id'=>$etudiant->getId(),'nom'=>$etudiant->getNom(), 'prenom'=>$etudiant->getPrenom(), 'formation'=>$etudiant->getFormation()]);
         }
 
-        return $this->render('tuteur/gestion.html.twig',['etudiants'=>$tabEtudiants]);
+        return $this->render('tuteur/gestion.html.twig',['etudiants'=>$tabEtudiants, 'tuteur_nom'=>$tuteur_nom]);
     }
 
+
+
+       #[Route('/addStudentForm', name: 'addStudentForm')]
+    public function ajouterEtudiantForm(Request $request): Response{ 
+
+      return $this->render('tuteur/addStudentForm.html.twig');
+    }
+
+
+      #[Route('/etudiant/new', name: 'addStudent')]
+    public function ajouterEtudiant(Request $request): Response{ 
+
+        $tuteur_id = $request->getSession()->get('tuteur_id');
+
+        $repository = $this->em->getRepository(Tuteur::class);
+        
+        $tuteur = $repository->find($tuteur_id);
+
+        $tuteur_nom = $tuteur->getNom();
+
+        $message = "";
+
+      if(isset($_POST['submit'])){
+
+        $nom = $_POST['nom'];
+        $prenom = $_POST['prenom'];
+        $formation = $_POST['formation'];
+
+        $etudiant = new Etudiant();
+
+        $etudiant->setNom($nom);
+        $etudiant->setPrenom($prenom);
+        $etudiant->setFormation($formation);
+
+        $etudiant->setTuteur($tuteur);
+
+        try{
+        $this->em->persist($etudiant);
+
+        $this->em->flush();
+          
+        $message = "Etudiant ajouté avec succes";
+
+        }
+
+        catch (Exception $e){
+          $message = "Erreur lors de l'ajout de l'étudiant : " . $e->getMessage() ;
+        }
+
+      }
+
+        $etudiants = $tuteur->getEtudiants();
+        
+
+        $tabEtudiants = [];
+        
+
+        foreach ($etudiants as $etudiant){
+            array_push($tabEtudiants, ['id'=>$etudiant->getId(),'nom'=>$etudiant->getNom(), 'prenom'=>$etudiant->getPrenom(), 'formation'=>$etudiant->getFormation()]);
+        }
+
+
+      return $this->render('tuteur/gestion.html.twig',['message'=>$message, 'tuteur_nom'=>$tuteur_nom, 'etudiants'=>$tabEtudiants]); 
+    }
+
+
+
+
+      #[Route('/updateStudentForm', name: 'updateStudentForm')]
+    public function updateStudentForm(Request $request): Response{ 
+
+      return $this->render('tuteur/updateStudentForm.html.twig');
+    }
 
 
 
